a7a5b45e219bf4395d61abd5364acd03e9207a3c
nikersify <nikersifyyt@gmail.com>
2017-11-30 20:04:36 +0100

    optimization tweaks
    + fixed a typo again
    + added favicons
    + fixed some common linters' errors, e.g. moved encoding to top of head
    + replaced some static resources with compressed versions of them
    + minified css & js
    + moved normalize.css from inline to cdn

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ const fs = require('fs')
 
 const babelify = require('babelify')
 const browserify = require('browserify')
+const cleanCSS = require('gulp-clean-css')
 const del = require('del')
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
+const source = require('vinyl-source-stream')
+const streamify = require('gulp-streamify')
+const uglify = require('gulp-uglify')
 
 gulp.task('js', () => {
 	return browserify('./app/js/index.js')
@@ -21,7 +26,9 @@ gulp.task('js', () => {
 			console.log(e.message)
 			this.emit('end')
 		})
-		.pipe(fs.createWriteStream('public/index.js'))
+		.pipe(source('index.js'))
+		.pipe(streamify(uglify()))
+		.pipe(gulp.dest('public/'))
 })
 
 gulp.task('pug', done => {
@@ -29,17 +36,20 @@ gulp.task('pug', done => {
 		.pipe(pug().on('error', err => {
 			done(err.message)
 		}))
+		.pipe(htmlmin())
 		.pipe(gulp.dest('public'))
 })
 
 gulp.task('assets', async () => {
 	await del('public/assets')
-	return gulp.src('app/assets/**/*').pipe(gulp.dest('public/assets'))
+	return gulp.src('app/assets/**/*')
+		.pipe(gulp.dest('public/assets'))
 })
 
 gulp.task('sass', () => {
 	return gulp.src('app/sass/*.sass')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS())
 		.pipe(gulp.dest('public'))
 })
 
